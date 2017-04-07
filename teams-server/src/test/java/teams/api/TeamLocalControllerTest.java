5a121dad4698e3f63afe73b7e8cbfe50225cea83
oharsta <oharsta@zilverline.com>
2017-04-07 15:39:24 +0200

    Better test for non-viewable teams

--- teams-server/src/test/java/teams/api/TeamLocalControllerTest.java
+++ teams-server/src/test/java/teams/api/TeamLocalControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import teams.Seed;
 import teams.domain.FederatedUser;
@@ -15,16 +14,14 @@ import teams.domain.TeamSummary;
 import teams.repository.TeamRepository;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 
 @RunWith(MockitoJUnitRunner.class)
-public class TeamLocalControllerTest implements Seed{
+public class TeamLocalControllerTest implements Seed {
 
     @Mock
     private TeamRepository teamRepository;
@@ -34,11 +31,13 @@ public class TeamLocalControllerTest implements Seed{
 
     @Test
     public void testSearchWithNonViewableTeam() throws Exception {
-        Team team = team(false);
+        Team notViewableTeam = team(false);
+        Team memberTeam = team(false);
         Person person = person("jdoe");
-        team.getMemberships().add(membership(Role.ADMIN,team, person));
+        membership(Role.ADMIN, memberTeam, person);
 
-        when(teamRepository.findByNameContainingIgnoreCaseOrderByNameAsc("q")).thenReturn(singletonList(team));
+        when(teamRepository.findByNameContainingIgnoreCaseOrderByNameAsc("q"))
+            .thenReturn(Arrays.asList(notViewableTeam, memberTeam));
 
         List<TeamSummary> summaries = teamController.teamSearch("q", new FederatedUser(person));
         assertEquals(1, summaries.size());
