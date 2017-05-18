bcfd303d226ac65bda4edd16fd9d13f50e36abd5
oharsta <oharsta@zilverline.com>
2017-05-18 11:02:48 +0200

    Delete obsolete external_teams during foreign key migration

--- teams-server/src/main/resources/db/migration/mysql/V9__add_foreign_key_team_external_groups.sql
+++ teams-server/src/main/resources/db/migration/mysql/V9__add_foreign_key_team_external_groups.sql
@@ -5,7 +5,7 @@ UPDATE team_external_groups AS team_external_groups
 SET team_external_groups.team_id = (SELECT teams.id
                                     FROM teams
                                     WHERE teams.urn = team_external_groups.grouper_team_id);
-
+DELETE FROM team_external_groups WHERE team_id IS NULL;
 ALTER TABLE team_external_groups
   MODIFY COLUMN team_id MEDIUMINT NOT NULL;
 ALTER TABLE team_external_groups
