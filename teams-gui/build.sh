a6f0034757aa03c1c75662318a18e448db8dab7b
oharsta <oharsta@zilverline.com>
2018-02-28 12:57:45 +0100

    Run lint and tests before deployment

--- teams-gui/build.sh
+++ teams-gui/build.sh
@@ -2,5 +2,5 @@
 echo "zipping and uploading"
 rm -Rf dist/*
 rm -Rf target/*
-##yarn install && yarn lint && yarn test && yarn run webpack
-npm install && npm run webpack
+yarn install && yarn lint && yarn test && yarn run webpack
+#npm install && npm run webpack
