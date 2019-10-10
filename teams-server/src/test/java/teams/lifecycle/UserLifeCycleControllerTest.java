909c07fe777f156278f6932b31de8b20821e2d80
Okke Harsta <oharsta@zilverline.com>
2019-10-10 17:18:41 +0200

    Test for non-existent urn

--- teams-server/src/test/java/teams/lifecycle/UserLifeCycleControllerTest.java
+++ teams-server/src/test/java/teams/lifecycle/UserLifeCycleControllerTest.java
@@ -3,15 +3,11 @@ package teams.lifecycle;
 import org.junit.Test;
 import teams.AbstractApplicationTest;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UserLifeCycleControllerTest extends AbstractApplicationTest {
 
@@ -43,35 +39,41 @@ public class UserLifeCycleControllerTest extends AbstractApplicationTest {
 
     @Test
     public void dryRun() {
-        LifeCycleResult result = doDeprovision(true);
+        LifeCycleResult result = doDeprovision(true, this.personUrn);
         assertLifeCycleResult(result);
     }
 
     @Test
     public void deprovision() {
-        LifeCycleResult result = doDeprovision(false);
+        LifeCycleResult result = doDeprovision(false, this.personUrn);
         assertLifeCycleResult(result);
 
-        result = doDeprovision(false);
+        result = doDeprovision(false, this.personUrn);
         assertEquals(0, result.getData().size());
     }
 
-    private LifeCycleResult doDeprovision(boolean dryRun) {
+    @Test
+    public void deprovisionNonExistentUser() {
+        LifeCycleResult result = doDeprovision(false, "nope");
+        assertEquals(0, result.getData().size());
+    }
+
+    private LifeCycleResult doDeprovision(boolean dryRun, String personUrn) {
         return given()
                 .auth()
                 .preemptive()
                 .basic("life", "secret")
                 .when()
-                .delete("deprovision/{user}" + (dryRun ? "/dry-run" : ""), this.personUrn)
+                .delete("deprovision/{user}" + (dryRun ? "/dry-run" : ""), personUrn)
                 .as(LifeCycleResult.class);
     }
 
     private void assertLifeCycleResult(LifeCycleResult result) {
         Map<String, String> map = result.getData().stream().collect(toMap(attr -> attr.getName(), attr -> attr.getValue()));
-        assertEquals(6 ,map.size());
+        assertEquals(6, map.size());
         assertEquals(map.get("email"), "unhappy@domain.net");
         assertEquals(map.get("joinRequest"), "orphans");
-        assertEquals(map.get("lastLoginDate").substring(0,10), "2018-05-17");
+        assertEquals(map.get("lastLoginDate").substring(0, 10), "2018-05-17");
         assertEquals(map.get("membership"), "orphans");
         assertEquals(map.get("name"), "Unhappy User");
         assertEquals(map.get("urn"), "urn:collab:person:example.com:unhappy");
