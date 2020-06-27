93aa585dd1d60b7131199596f3e77ae4d5e8f296
Okke Harsta <oharsta@zilverline.com>
2020-06-27 17:05:08 +0200

    Introduced language constants

---
+++ teams-gui/src/javascripts/constants/languages.js
@@ -0,0 +1,9 @@
+import nlFlag from "../../images/nl_flag.png";
+import enFlag from "../../images/en_flag.png";
+import ptFlag from "../../images/pt_flag.png";
+
+export const languageOptions = [
+  {value: "DUTCH", label: "Nederlands", src: nlFlag, code: "nl"},
+  {value: "ENGLISH", label: "English", src: enFlag, code: "en"},
+  {value: "PORTUGUESE", label: "Português", src: ptFlag, code: "pt"}
+];
