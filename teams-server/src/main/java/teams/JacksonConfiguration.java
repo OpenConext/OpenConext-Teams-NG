68d74a8fc599f5baa844cd7245547bf0301b622d
Okke Harsta <oharsta@zilverline.com>
2022-02-17 14:02:40 +0100

    Bugifx for Jackson configuration

--- teams-server/src/main/java/teams/JacksonConfiguration.java
+++ teams-server/src/main/java/teams/JacksonConfiguration.java
@@ -1,27 +1,26 @@
 package teams;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JacksonConfiguration {
 
     @Bean
-    public Hibernate5Module hibernate5Module() {
-        return new Hibernate5Module();
-    }
-
-    @Bean
-    public Jdk8Module jdk8Module() {
-        return new Jdk8Module();
-    }
-
-    @Bean
-    public JavaTimeModule javaTimeModule() {
-        return new JavaTimeModule();
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new Hibernate5Module());
+        mapper.registerModule(new Jdk8Module());
+        return mapper;
     }
 
 }
