a59f46360cfa40e35c907e0ef165033afd9e0a25
Okke Harsta <oharsta@zilverline.com>
2022-04-20 14:07:15 +0200

    Synced locales and added tests to run before deploy

--- teams-redesign/src/locale/en.js
+++ teams-redesign/src/locale/en.js
@@ -1,8 +1,3 @@
-// Interpolation works as follows:
-//
-// Make a key with the translation and enclose the variable with {{}}
-// ie "Hello {{name}}" Do not add any spaces around the variable name.
-// Provide the values as: I18n.t("key", {name: "John Doe"})
 import I18n from "i18n-js";
 
 I18n.translations.en = {
--- teams-redesign/src/locale/nl.js
+++ teams-redesign/src/locale/nl.js
@@ -1,21 +1,61 @@
-// Interpolation works as follows:
-//
-// Make a key with the translation and enclose the variable with {{}}
-// ie "Hello {{name}}" Do not add any spaces around the variable name.
-// Provide the values as: I18n.t("key", {name: "John Doe"})
 import I18n from "i18n-js";
 
 I18n.translations.nl = {
     code: "NL",
     myteams: {
-        title: "Title",
-        members: "Members",
+        columns: {
+            title: "Title",
+            members: "Members",
+            private: "",
+            member: "",
+            bin: ""
+        },
         empty: "",
-        private:"private"
+        private: "Private team",
+        add_members: "Add members"
     },
     profile: {
         admin: "Admin",
-        guest: "Guest"
+        guest: "Guest",
+        email: "Email",
+        name: "Name",
+        role: "Role",
+        true: "{{productName}} guest",
+        false: "{{productName}} admin"
+    },
+    roles: {
+        guest: "Guest",
+        member: "Member",
+        manager: "Manager",
+        admin: "Admin",
+        owner: "Owner",
+        title: "You're {{role}}"
+    },
+    details: {
+        leave: "Leave team",
+        delete: "Delete team",
+        confirmations: {
+            leave: "Are you sure you want to leave this team?",
+            delete: "Are you sure you want to delete this team?"
+        }
+    },
+    breadcrumbs: {
+        myTeams: "My teams"
+    },
+    confirmationDialog: {
+        title: "Please confirm",
+        confirm: "Confirm",
+        cancel: "Cancel",
+        questions: {
+            delete: "Are you sure you want to delete {{object}} {{name}}?"
+        }
+    },
+    footer: {
+        faq: "<a href='https://surf.nl' target='_blank' referrerpolicy='no-referrer'>FAQ</a>",
+        terms: "<a href='https://surf.nl' target='_blank' referrerpolicy='no-referrer'>Terms of use</a>",
+        privacy: "<a href='https://surf.nl' target='_blank' referrerpolicy='no-referrer'>Privacy policy</a>",
+        powered: "Proudly powered by",
+        surf: "<a href='https://surf.nl' target='_blank' referrerpolicy='no-referrer'>SURF</a>",
     }
 };
 
