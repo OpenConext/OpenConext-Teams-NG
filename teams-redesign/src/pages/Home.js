5edceb9a7d648244ac56ce4217c447b3b8feaa59
Samantha Whitehead <sam@Samanthas-MacBook-Pro.local>
2022-04-22 11:51:36 +0200

    restore home page

--- teams-redesign/src/pages/Home.js
+++ teams-redesign/src/pages/Home.js
@@ -1,14 +1,9 @@
-
-import { Tabs,Tab } from "../components/Tabs";
 import "./Home.scss";
 
 const Home = ({user}) => (
     <div className="home">
-        <Tabs>
-            <Tab title={1}><div>uhh</div></Tab>
-            <Tab title={"hahaha"}>2</Tab>
-            <Tab title={"oooooooo"}>3</Tab>
-        </Tabs>
+        <p>{JSON.stringify(user)}</p>
+        <p>TODO</p>
     </div>
 
 );
