103a2a0e60bc0b1f0c86ae9eebcc447bc6e9276c
Okke Harsta <oharsta@zilverline.com>
2022-05-20 16:58:32 +0200

    Changed colors according design system

--- teams-gui/src/components/Tooltip.js
+++ teams-gui/src/components/Tooltip.js
@@ -2,13 +2,12 @@ import React from "react";
 import informational from "../icons/alert-information-circle.svg";
 import "./Tooltip.scss";
 import Tippy from '@tippyjs/react';
-// import 'tippy.js/dist/tippy.css';
 
 const TooltipIcon = ({tooltip, name, label}) => {
     return <>
             <span className="tool-tip-section">
                 <label htmlFor={name}>{label}</label>
-                 <Tippy interactive={true} content={<span dangerouslySetInnerHTML={{
+                 <Tippy content={<span dangerouslySetInnerHTML={{
                      __html: tooltip
                  }}/>}>
                     <img src={informational} alt="info"/>
